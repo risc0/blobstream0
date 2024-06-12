e9f5c3f100e223a9cdc51c413705464768fd4174
Austin Abell <austinabell8@gmail.com>
2024-06-11 21:36:07 -0400

    implement guest verification logic and preflighting

--- guest/src/main.rs
+++ guest/src/main.rs
@@ -1,7 +1,39 @@
+use core::time::Duration;
 use risc0_zkvm::guest::env;
+use tendermint_light_client_verifier::{
+    options::Options, types::LightBlock, ProdVerifier, Verdict, Verifier,
+};
 
 fn main() {
-    let input: u32 = env::read();
+    let (light_block_previous, light_block_next): (LightBlock, LightBlock) =
+        ciborium::from_reader(env::stdin()).unwrap();
 
-    env::commit(&input);
+    let vp = ProdVerifier::default();
+    let opt = Options {
+        trust_threshold: Default::default(),
+        // TODO check these options (value pulled from TM repo)
+        trusting_period: Duration::from_secs(60),
+        clock_drift: Default::default(),
+    };
+    // TODO this verify time picked pretty arbitrarily, need to be after header time and within
+    // trusting window.
+    let verify_time = light_block_next.time() + Duration::from_secs(20);
+    let verdict = vp.verify_update_header(
+        // TODO should assert that next_validators is Some
+        light_block_next.as_untrusted_state(),
+        light_block_previous.as_trusted_state(),
+        &opt,
+        verify_time.unwrap(),
+    );
+
+    // TODO check trusted.next_validators.hash() == trusted.next_validators_hash (might be done implicitly)
+
+    assert!(
+        matches!(verdict, Verdict::Success),
+        "validation failed, {:?}",
+        verdict
+    );
+
+    env::commit_slice(light_block_previous.signed_header.header.hash().as_bytes());
+    env::commit_slice(light_block_next.signed_header.header.hash().as_bytes());
 }
--- host/src/main.rs
+++ host/src/main.rs
@@ -1,17 +1,40 @@
 use risc0_zkvm::{compute_image_id, default_prover, ExecutorEnv};
+use tendermint::{block::Height, node::Id, validator::Set};
+use tendermint_light_client_verifier::types::LightBlock;
+use tendermint_rpc::{Client, HttpClient, Paging};
 
 const ELF: &[u8] = include_bytes!("../../target/riscv32im-risc0-zkvm-elf/release/guest");
+const CELESTIA_RPC_URL: &str = "https://rpc.celestia-mocha.com";
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
         .init();
 
-    let guest_id = compute_image_id(ELF).unwrap();
+    let client = HttpClient::new(CELESTIA_RPC_URL)?;
+    let commit = client.latest_commit().await?;
 
-    let input: u32 = 15 * u32::pow(2, 27) + 1;
-    let env = ExecutorEnv::builder().write(&input)?.build()?;
+    // Fetch latest commit just for block number.
+    let block_num = commit.signed_header.header.height;
+    println!("fetching blocks at height: {}", block_num);
+
+    // Retrieve past blocks
+    let next_block = fetch_light_block(&client, block_num).await?;
+    // We could request further back, as long as no more than 1/3 validators rotated between,
+    // but for this use case, we will be validating every block.
+    let previous_block =
+        fetch_light_block(&client, Height::try_from(block_num.value() - 2)?).await?;
+
+    // TODO remove the need to serialize with cbor
+    let mut input_serialized = Vec::new();
+    ciborium::into_writer(&(&previous_block, &next_block), &mut input_serialized)?;
+
+    let guest_id = compute_image_id(ELF)?;
+    let env = ExecutorEnv::builder()
+        .write_slice(&input_serialized)
+        .build()?;
 
     let prover = default_prover();
 
@@ -22,3 +45,29 @@ fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+async fn fetch_light_block(
+    client: &HttpClient,
+    block_height: Height,
+) -> anyhow::Result<LightBlock> {
+    let commit_response = client.commit(block_height).await?;
+    let signed_header = commit_response.signed_header;
+    let height = signed_header.header.height;
+
+    // Note: This currently needs to use Paging::All or the hash mismatches.
+    let validator_response = client.validators(height, Paging::All).await?;
+
+    let validators = Set::new(validator_response.validators, None);
+
+    let next_validator_response = client.validators(height.increment(), Paging::All).await?;
+    let next_validators = Set::new(next_validator_response.validators, None);
+
+    Ok(LightBlock::new(
+        signed_header,
+        validators,
+        next_validators,
+        // TODO do we care about this ID?
+        Id::new([0; 20]),
+    ))
+}
+// }
