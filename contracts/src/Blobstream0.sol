27370b1a71cabd201b881f3c271852d8fa7d3a2c
Austin Abell <austinabell8@gmail.com>
2024-08-19 15:44:15 -0700

    implement ownership pattern for contract

--- contracts/src/Blobstream0.sol
+++ contracts/src/Blobstream0.sol
@@ -19,12 +19,13 @@ pragma solidity ^0.8.20;
 import {IRiscZeroVerifier} from "risc0/IRiscZeroVerifier.sol";
 import {ImageID} from "./ImageID.sol"; // auto-generated contract after running `cargo build`.
 import {IDAOracle} from "blobstream/IDAOracle.sol";
+import {Ownable, Ownable2Step} from "openzeppelin/contracts/access/Ownable2Step.sol";
 import "./RangeCommitment.sol";
 import "blobstream/DataRootTuple.sol";
 import "blobstream/lib/tree/binary/BinaryMerkleTree.sol";
 
 /// @title A starter application using RISC Zero.
-contract Blobstream0 is IDAOracle {
+contract Blobstream0 is IDAOracle, Ownable2Step {
     /// @notice Data commitment stored for the block range [startBlock, endBlock) with proof nonce.
     /// NOTE: This event matches existing Blobstream contracts, for ease of integration.
     /// @param proofNonce The nonce of the proof.
@@ -47,14 +48,14 @@ contract Blobstream0 is IDAOracle {
     error InvalidTrustedHeaderHash();
 
     /// @notice RISC Zero verifier contract address.
-    IRiscZeroVerifier public immutable verifier;
+    IRiscZeroVerifier public verifier;
 
     /// @notice Image ID of the only zkVM binary to accept verification from.
     ///         The image ID is similar to the address of a smart contract.
     ///         It uniquely represents the logic of that guest program,
     ///         ensuring that only proofs generated from a pre-defined guest program
     ///         (in this case, checking if a number is even) are considered valid.
-    bytes32 public constant imageId = ImageID.BATCH_GUEST_ID;
+    bytes32 public imageId = ImageID.BATCH_GUEST_ID;
 
     uint256 public proofNonce;
 
@@ -71,12 +72,30 @@ contract Blobstream0 is IDAOracle {
     mapping(uint256 => bytes32) merkleRoots;
 
     /// @notice Initialize the contract, binding it to a specified RISC Zero verifier.
-    constructor(IRiscZeroVerifier _verifier, bytes32 _trustedHash, uint64 _trustedHeight) {
+    constructor(address _admin, IRiscZeroVerifier _verifier, bytes32 _trustedHash, uint64 _trustedHeight)
+        Ownable(_admin)
+    {
         verifier = _verifier;
         latestBlockHash = _trustedHash;
         latestHeight = _trustedHeight;
 
         proofNonce = 1;
+    }
+
+    /// @notice Only the admin can update. Updates the trusted height and block hash to sync from.
+    function adminSetTrustedState(bytes32 _trustedHash, uint64 _trustedHeight) external onlyOwner {
+        latestBlockHash = _trustedHash;
+        latestHeight = _trustedHeight;
+    }
+
+    /// @notice Only the admin can update. Updates the image ID to verify proofs against.
+    function adminSetImageId(bytes32 _imageId) external onlyOwner {
+        imageId = _imageId;
+    }
+
+    /// @notice Only the admin can update. Updates the verifier contract address.
+    function adminSetVerifier(IRiscZeroVerifier _verifier) external onlyOwner {
+        verifier = _verifier;
     }
 
     /// @notice Validate a proof of a new header range, update state.
