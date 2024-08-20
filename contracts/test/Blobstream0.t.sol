35ee6d9d78c84d0e32e8e812f632fa6c24ec4ac2
Austin Abell <austinabell8@gmail.com>
2024-08-19 18:20:42 -0700

    add test to build verifier contracts

---
+++ contracts/test/Blobstream0.t.sol
@@ -0,0 +1,26 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.20;
+
+import {RiscZeroMockVerifier} from "risc0/test/RiscZeroMockVerifier.sol";
+import {RiscZeroGroth16Verifier} from "risc0/groth16/RiscZeroGroth16Verifier.sol";
+
+contract Blobstream0Test  {
+	// NOTE: This is only used to build these contracts to be used for the CLI and tests, ignore
+	RiscZeroGroth16Verifier a;
+	RiscZeroMockVerifier b;
+}
